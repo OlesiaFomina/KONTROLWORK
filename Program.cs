e0f2dc5fcc95b46cf8113ae6691a001df19b2e8e
Fomina Olesia <olesyafomina03@gmail.com>
2023-11-09 20:56:56 +0300

    добавлен вывод команд для выбора

---
+++ Program.cs
@@ -0,0 +1,44 @@
+ListOfCommands();
+string[] array = new string[] {};
+
+string inputNum = ReadInput("Введите номер команды: ");
+switch (inputNum)
+{
+    case "1":
+        array = new string[] { "Hello", "2", "world", ":-)" };
+        break;
+    case "2":
+        array = new string[] { "1234", "1567", "-2", "computer science" };
+        break;
+    case "3":
+        array = new string[] { "Russia", "Denmark", "Kazan" };
+        break;
+    default:
+        Console.WriteLine($"{inputNum} - Команда указана неверно!");
+        break;
+}
+
+
+int lengNewArray = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3) lengNewArray++;
+}
+
+string[] newArray = new string[lengNewArray];
+int a = 0;
+
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[a] = array[i];
+        a++;
+    }
+}
+
+PrintArray(array);
+Console.Write("→ ");
+PrintArray(newArray);
+
+void ListOfCommands()
